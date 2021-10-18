db20a7ee900c21c6c4dc6d724270d1ab85511c3b
gba <azaz332733@gmail.com>
2021-10-18 02:17:55 -0500

    Terminado, haciendo pruebas

--- Agente explorador/agente_explorador.m
+++ Agente explorador/agente_explorador.m
@@ -10,19 +10,24 @@ close all
 
 total_des = 1000; %Decisines totales por ambiente
 total_agentes = 20; %total de Agentes
-punt =zeros(1,total_agentes); %Puntuacion para total de agentes
+%punt =zeros(1,total_agentes); %Puntuacion para total de agentes
 %Decisiones aleatorias para el total de agentes agentes
 %acciones=zeros(total_agentes,244);
 acciones= round(0 + (6-0).*rand(total_agentes,244));
-
+%cantidad de mutaciones
+mut_amount =5;
 % acciones=zeros(20,244);
 % acciones = acciones + 4;
+
+for etapa=1:1000
+punt =zeros(1,total_agentes); 
 
 %%Para los k agentes
 for k=1: total_agentes 
 %%Iterar en 10 ambientes distintos    
 for v=1:10 
     plane=crearAmbiente();
+    
     %%Iniciar Agente en posición aleatoria
     a=2;
     b=11;
@@ -131,21 +136,76 @@ for d=1: total_des %%1000 decisiones
 end
 end
 end
-punt
+%punt;
 %%TODO
 %%%%SLECCION CON PUNT Y ACCIONES
 
 %Fitness value
 %Normalizar cada valor entre 0 y 1
-max=500;
+max=10000;
 min=-30000;
 punt_norm = (punt - min) .* (1/(max-min));
 
 %Normalizar rango 0 a 1
 punt_norm2 = punt_norm .* (1/sum(punt_norm));
 
-punt_norm2Sum = movsum(punt_norm2,[20 0])
+punt_norm2Sum = movsum(punt_norm2,[20 0]);
 
 %%TODO aleatorio comparar con punt_norm2Sum
-p = rand();
+for k=1:size(punt,2)
+    p = rand();
+    %%Seleccionar el intervalo del número aleatorio
+    temp=[0 punt_norm2Sum];
+    i=1;
+    j=size(temp, 2);
+    m= ceil(j/2);
+    while size(temp,2) >2
+        if p > temp(1,m)
+            i=m;
+            j=size(temp, 2);
+        else 
+            i=1;
+            j=m;
+        end
+        temp = temp(1,[i:j]);
+        j=size(temp, 2);
+        m=ceil(j/2);
+    end
+    %%%Indice del elemento seleccionado
+    idx(k)=find(punt_norm2Sum==temp(1,2));
+    acciones_new(k,:)= acciones(idx(k),:)  ;
+end
+idx;
+%% Cruzamiento
+%1 punto
 
+
+for k=1:size(acciones_new,1)-1
+    a=1;
+    b=243;
+    cross_point= round(a + (b-a).*rand());%x
+
+    padre1= acciones_new(k,:);
+    padre2= acciones_new(k+1,:);
+
+    hijo1 = padre1(1,[1:cross_point-1]) ;
+    hijo1 = cat(2, hijo1, padre2(1,[cross_point:end]));    
+    hijo2 = padre2(1,[1:cross_point-1]) ;
+    hijo2 = cat(2, hijo2, padre1(1,[cross_point:end]));
+    
+    %%%Mutacion
+    mut_points1= round(a + (b-a).*rand(1,mut_amount));%
+    mut_points2= round(a + (b-a).*rand(1,mut_amount));
+    for i=1:size(mut_points1,2)
+        alelo_new=round(0 + (6-0).*rand());
+        hijo1(mut_points1(1,i))= alelo_new;
+        alelo_new=round(0 + (6-0).*rand());
+        hijo2(mut_points2(1,i))= alelo_new;
+    end
+    
+    hijos(k,:) = hijo1;
+    hijos(k+1,:) =hijo2;
+end
+    
+acciones = hijos;
+end
